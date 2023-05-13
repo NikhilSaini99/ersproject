0219209ec9ea94fcfdd3d1236c706d894ce73560
Nikhil Saini <nikhilsaini42@gmail.com>
2023-05-13 17:05:44 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        mainColor: "#2F3192",
+        subColor: "#17197C",
+        yellowish: "#FFC43A",
+        normalBlue: '#2251FF'
+      },
+      height:{
+        512:'512px'
+      },
+      width:{
+        20:'20%',
+        50:"60%",
+        65:"85%"
+      },
+    },
+  },
+  plugins: [
+    require('tailwindcss-textshadow'),
+    // require('@tailwindcss/typography')
+  ],
+}
